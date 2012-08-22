299e6ada3af06c42561f1a96b19b321854edef31
David Corvoysier <david.corvoysier@orange.com>
2012-08-22 16:17:21 +0200

    Called subscript instead of duplicating command

--- update-all-json.sh
+++ update-all-json.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <path-to-webkit-git-repo>"
+    exit
+fi
 BASEDIR=$(dirname $0)
 for ((i=2001; i<=$(date "+%Y");i++))
 do
-$BASEDIR/git-log-to-json.py -s "${i}-01-01" -u "${i}-12-31" -p $i
+$BASEDIR/update-json.sh $1 $i
 done
 
