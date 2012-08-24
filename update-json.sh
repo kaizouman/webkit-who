b2d08cf83ab802eed64b5fef44d327b6a6bf7b00
David Corvoysier <david.corvoysier@orange.com>
2012-08-24 17:11:52 +0200

    Fixed date parsing bug when specifying a range whose 'until' date is in the future (in that case the date is parsed by git the european way)

--- update-json.sh
+++ update-json.sh
@@ -3,11 +3,17 @@ if [ $# -lt 1 ]; then
     echo "Usage: $0 <path-to-webkit-git-repo> [year]"
     exit
 fi
-if [ $2 != '' ]; then
+if [ $# -eq 2 ]; then
 year=$2
 else
 year=$(date "+%Y")
 fi
+Since="${year}-01-01"
+if [ $year == $(date "+%Y") ]; then
+Until="HEAD"
+else
+Until="${year}-12-31"
+fi
 BASEDIR=$(dirname $0)
-GIT_DIR=$1/.git GIT_WORK_TREE=$1 $BASEDIR/git-log-to-json.py -s "${year}-01-01" -u "${year}-12-31" -p $year
+GIT_DIR=$1/.git GIT_WORK_TREE=$1 $BASEDIR/git-log-to-json.py -s $Since -u $Until -p $year
 
